a035a7a1ad5195989e55793de7a1da404c01ae2d
nisshthaa <sharmanishtha791@gmail.com>
2026-02-20 13:56:35 +0530

    added js to table

--- script.js
+++ script.js
@@ -13,18 +13,14 @@ add_btn.addEventListener("click", function (e) {
 });
 
 document.addEventListener("click", function (e) {
-
   // If form is open
   if (form_data.style.display === "block") {
-
     // Check if click is outside the form AND not the add button
     if (!form_data.contains(e.target) && !add_btn.contains(e.target)) {
-      
       form_data.style.display = "none";
       main.style.filter = "none";
     }
   }
-
 });
 
 // delete-items
@@ -44,20 +40,18 @@ form.addEventListener("submit", function (e) {
 
   let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
 
-  if(edit_index!=null){
-    userData[edit_index]={
-    title: title,
-    status: status
-  };
-  editIndex = null;
-  }
-else{
+  if (edit_index != null) {
+    userData[edit_index] = {
+      title: title,
+      status: status,
+    };
+    editIndex = null;
+  } else {
     userData.push({
-    title: title,
-    status: status,
-  });
-}
-
+      title: title,
+      status: status,
+    });
+  }
 
   localStorage.setItem("userDetails", JSON.stringify(userData));
 
@@ -75,6 +69,7 @@ let displayData = () => {
   let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
   let finalData = "";
   let date = new Date().toLocaleDateString();
+  let time = new Date().toLocaleTimeString();
   let button = "";
   let btn_class = "";
 
@@ -88,16 +83,16 @@ let displayData = () => {
     } else {
       button = "Download PDF";
       btn_class = "completed";
-      btn_status = "completed";
     }
 
     finalData += `
-          <tr class="tr-style">
+          <tr>
           <td ><input type="checkbox"></td>
           <td class="td-items">${element.title}</td>
           <td class="td-items ${btn_class}">${element.status}</td>
-          <td class="td-items date-text">${date}</td>
-          <td class="btn-status">${button}</td>
+          <td class=" date-text">${date}<br>${time}</td>
+          <td class=" btn-status">${button}</td>
+
           <td class="settings-wrapper">
             <img src="./assets/more_vert_24dp_5F6368_FILL0_wght400_GRAD0_opsz24 2.svg"
               alt="settings"
@@ -109,16 +104,11 @@ let displayData = () => {
           </td>
           </tr>
           `;
-
-    
-  }
-
-);
-tbody.innerHTML = finalData;
+  });
+  tbody.innerHTML = finalData;
 };
 
 displayData();
-
 
 document.addEventListener("click", function (e) {
   //close all existing ones
@@ -143,40 +133,95 @@ document.addEventListener("click", function (e) {
   }
 });
 
-
 //edit items
-let edit_index=null
-let title_doc=document.getElementById("title_doc")
-let status_id=document.getElementById("status_id")
+let edit_index = null;
+let title_doc = document.getElementById("title_doc");
+let status_id = document.getElementById("status_id");
 
-document.addEventListener("click",function(e){
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("edit")) {
+    let index = e.target.getAttribute("data-index");
+    let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
 
- if(e.target.classList.contains("edit")){
-  let index = e.target.getAttribute("data-index");
-  let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
-
-  let item=userData[index]
-  title_doc.value=item.title
-  status_id.value=item.status
-  edit_index=index
-   main.style.filter = "blur(10px)";
-  form_data.style.display = "block";
-
- } 
-})
-
+    let item = userData[index];
+    title_doc.value = item.title;
+    status_id.value = item.status;
+    edit_index = index;
+    main.style.filter = "blur(10px)";
+    form_data.style.display = "block";
+  }
+});
 
 //delete items
 
-document.addEventListener("click",function(e){
-  if(e.target.classList.contains("delete")){
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
     let index = e.target.getAttribute("data-index");
     let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
- // Remove that item
+    // Remove that item
     userData.splice(index, 1);
 
     localStorage.setItem("userDetails", JSON.stringify(userData));
     displayData();
-
   }
-})
+});
+
+//searchbar functionallity
+
+let searchBox = document.querySelector("#search-box");
+
+searchBox.addEventListener("keyup", function () {
+  const searchValue = searchBox.value.toLowerCase();
+  let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+
+  let filtered_data = userData.filter((item) => {
+    return (
+      item.title.toLowerCase().includes(searchValue) ||
+      item.status.toLowerCase().includes(searchValue)
+    );
+  });
+  displayFilteredData(filtered_data);
+});
+
+function displayFilteredData(data) {
+  let finalData = "";
+  let date = new Date().toLocaleDateString();
+  let time = new Date().toLocaleTimeString();
+  let button = "";
+  let btn_class = "";
+
+  data.forEach((element, i) => {
+    if (element.status === "Needs Signing") {
+      button = "Sign Now";
+      btn_class = "signNow";
+    } else if (element.status === "Pending") {
+      button = "Preview";
+      btn_class = "pending";
+    } else {
+      button = "Download PDF";
+      btn_class = "completed";
+      btn_status = "completed";
+    }
+
+    finalData += `
+          <tr class="tr-style">
+          <td ><input type="checkbox"></td>
+          <td class="td-items">${element.title}</td>
+          <td class="td-items ${btn_class}">${element.status}</td>
+          <td class="td-items date-text">${date}<br>${time}</td>
+          <td class="btn-status">${button}</td>
+          
+          <td class="settings-wrapper">
+            <img src="./assets/more_vert_24dp_5F6368_FILL0_wght400_GRAD0_opsz24 2.svg"
+              alt="settings"
+              class="settings-icon">
+            <div class="settings-div">
+              <button class="edit" data-index="${i}">Edit</button>
+              <button class="delete" data-index="${i}">Delete</button>
+            </div>
+          </td>
+          </tr>
+          `;
+  });
+  tbody.innerHTML = finalData;
+}
